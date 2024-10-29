8a0c17108ce25f6f915e7c9e4a62ca33c50d94c2
Marine <marinekervella@yahoo.fr>
2024-10-29 15:37:20 +0100

    css fridgeScreen

--- screens/FridgeScreen.js
+++ screens/FridgeScreen.js
@@ -1,105 +1,191 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-
-
 
 const FridgeScreen = () => {
   const navigation = useNavigation();
-    const handleFreezerPress = () => {
-        {/* Naviguer vers la page du congélateur */}
-        navigation.navigate('FreezerScreen');
-      };
-      const handleCeboardPress = () => {
-        {/* Naviguer vers la page du placard */}
-        navigation.navigate('PantryScreen ');
-      };
+  const handleFreezerPress = () => {
+    {
+      /* Naviguer vers la page du congélateur */
+    }
+    navigation.navigate("FreezerScreen");
+  };
+  const handleCeboardPress = () => {
+    {
+      /* Naviguer vers la page du placard */
+    }
+    navigation.navigate("PantryScreen ");
+  };
 
   return (
     <View style={styles.container}>
-        <Image source={require('../assets/Squirrel/Heureux.png')} style={styles.squirrel}/>
+      <Image
+        source={require("../assets/Squirrel/Heureux.png")}
+        style={styles.squirrel}
+      />
 
       <Text style={styles.PageTitle}>Contenu de mon Frigo</Text>
       <View style={styles.productContainer}>
         {/* Affichage des produits */}
-        <Text style={styles.ProductTitle}>Produit 1</Text>
-        <Text style={styles.DLCDate}>DLC</Text>
-        <Image source={require("../assets/FRIGO.png")} style={styles.frigoLogo} />
-        
-        <Text style={styles.ProductTitle}>Produit 2</Text>
-        <Text style={styles.DLCDate}>DLC</Text>
-        <Image source={require("../assets/FRIGO.png")} style={styles.frigoLogo} />
-
-        <Text style={styles.ProductTitle}>Produit 3</Text>
-        <Text style={styles.DLCDate}>DLC</Text>
-          <Image source={require("../assets/FRIGO.png")} style={styles.frigoLogo} />
+        <View style={styles.ProductLineContainer}>
+          <Text style={styles.ProductTitle}>Produit 1</Text>
+          <View style={styles.DlcContainer}>
+          <Text style={styles.DlcText}>DLC</Text>
+          </View>
+          <View style={styles.buttonFreezer}>
+          <Image
+            source={require("../assets/congelo.png")}
+            style={styles.freezerLogo}
+          />
+        </View>
+        </View>
+        <View style={styles.ProductLineContainer}>
+          <Text style={styles.ProductTitle}>Produit 2</Text>
+          <View style={styles.DlcContainer}>
+          <Text style={styles.DlcText}>DLC</Text>
+          </View>
+          <View style={styles.buttonFreezer}>
+          <Image
+            source={require("../assets/congelo.png")}
+            style={styles.freezerLogo}
+          />
+        </View>
+        </View>
+        <View style={styles.ProductLineContainer}>
+          <Text style={styles.ProductTitle}>Produit 3</Text>
+          <View style={styles.DlcContainer}>
+          <Text style={styles.DlcText}>DLC</Text>
+          </View>
+          <View style={styles.buttonFreezer}>
+          <Image
+            source={require("../assets/congelo.png")}
+            style={styles.freezerLogo}
+          />
+        </View>
+        </View>
       </View>
 
       {/* Boutons d'accès au congélateur */}
       <View style={styles.stocksButtonsContainer}>
-      <TouchableOpacity style={styles.freezer} onPress={handleFreezerPress}>
-                <Text style={styles.buttonText}>Mon Congélo</Text>
-                
+        <TouchableOpacity style={styles.button} onPress={handleFreezerPress}>
+          <Text style={styles.buttonText}>Mon Congélo</Text>
         </TouchableOpacity>
-      {/* Boutons d'accès aux placards */}
-        <TouchableOpacity style={styles.ceboard} onPress={handleCeboardPress}>
-                <Text style={styles.buttonText}>Mes Placards</Text>
+        {/* Boutons d'accès aux placards */}
+        <TouchableOpacity style={styles.button} onPress={handleCeboardPress}>
+          <Text style={styles.buttonText}>Mes Placards</Text>
         </TouchableOpacity>
-
       </View>
-
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#EFE5D8',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    squirrel: {
-       
-        width: 60,
-        height: 60,
-        marginBottom: 10,
-        marginRight: 250,
-        marginTop: -55,
-    },
-    PageTitle:{
+  container: {
+    flex: 1,
+    backgroundColor: "#EFE5D8",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  squirrel: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    top: 65,
+    left: 30,
+  },
+  PageTitle: {
+    color: "#E56400",
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  productContainer: {
+    borderWidth: 1,
+    backgroundColor: "#FAF9F3",
+    borderColor: "#A77B5A",
+    width: "85%",
+    height: "65%",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+  },
+  ProductLineContainer: {
+    flexDirection:'row',
+    backgroundColor: "#FAF9F3",
+    borderColor: "#A77B5A",
+    borderWidth: 2,
+    width: 200,
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 16,
+    alignContent: 'space-between',
+    marginTop: 5,
+    marginBottom: 5,
+    
+  },
+  ProductTitle: {
+    justifyContent: "center",
+    fontSize: 15,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#E56400",
+  },
+  DlcContainer:{
+    justifyContent: "center",
+    backgroundColor: "#FAF9F3",
+    borderColor: "#A77B5A",
+    borderWidth: 2,
+    width: 50,
+    height: 50,
+    top:-11,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 16,
+    marginLeft: 125,
+    
+  },
+  DlcText: {
 
-    },
-    productContainer:{
-
-    },
-    ProductTitle: {
-        position: 'absolute',
-       marginTop: 20,
-        textAlign: 'center',
-        color: 'black',
-        fontSize: 20,
-        top: 0,
-        
-    },
-    tipsText: {
-        fontSize: 15,
-        textAlign: 'center',
-    },
-    DLCDate:{
-
-    },
-    frigoLogo:{
-
-    },
-    stocksButtonsContainer: {
-       
-        borderWidth: 1,
-        width: '85%',
-        height: '10%',
-        borderRadius: 10,
-        padding: 10,
-    },
-
+  },
+  buttonFreezer:{
+    justifyContent: "center",
+    backgroundColor: "#FAF9F3",
+    borderColor: "#A77B5A",
+    borderWidth: 2,
+    width: 50,
+    height: 50,
+    top:-11,
+    borderRadius: 10,
+    padding: 10,
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  freezerLogo: {
+    width: 30,
+    height: 30,
+  },
+  stocksButtonsContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    justifyContent: "center",
+    backgroundColor: "#FAF9F3",
+    borderColor: "#A77B5A",
+    borderWidth: 1,
+    width: 150,
+    height: 70,
+    borderRadius: 10,
+    padding: 10,
+    marginRight: 16,
+    marginLeft: 16,
+    fontSize: 16,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#E56400",
+  },
 });
 
 export default FridgeScreen;
