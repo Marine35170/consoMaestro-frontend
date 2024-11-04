f222c0592c7515512e0946b773b550cdb659a527
Your name <Your email>
2024-11-04 11:33:31 +0100

    modifs modales profilescreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -167,7 +167,7 @@ export default function ProfileScreen() {
         >
           <View style={styles.modalContainer}>
             <Text style={styles.modalTitle}>Mes Parrainages</Text>
-            <Text style={styles.modalContent}>Page en cours de construction 👷🏻‍♀️</Text>
+            <Text style={styles.modalContent}>Page en cours de construction 👷🏿‍♂️</Text>
             <TouchableOpacity style={styles.closeButton} onPress={() => setSponsorshipsModalVisible(false)}>
               <Text style={styles.closeButtonText}>Fermer</Text>
             </TouchableOpacity>
